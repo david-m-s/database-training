c8b1e9288b29141d4b42c18cffd8a2be5118d3c7
David Mas <dmas@magento.com>
2019-03-19 11:52:45 +0100

    Do not use field autowiring.

--- src/main/java/com/magento/example/service/UserService.java
+++ src/main/java/com/magento/example/service/UserService.java
@@ -2,14 +2,16 @@ package com.magento.example.service;
 
 import com.magento.example.domain.User;
 import com.magento.example.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-  @Autowired
   private UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
   public void addAll(User... users) {
     for (User u : users) {
